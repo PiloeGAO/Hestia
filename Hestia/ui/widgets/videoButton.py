6f7b3288d461d9c9022dd63bf9ff24a6b67389ad
PiloeGAO <leonumerique@gmail.com>
2021-07-09 09:41:44 +0200

    ADD: Initial implementation of vlc binding in videoButton widget.

--- Hestia/ui/widgets/videoButton.py
+++ Hestia/ui/widgets/videoButton.py
@@ -6,19 +6,25 @@
     :version:   0.0.4
 """
 import os
+import platform
 
 try:
     from PySide2.QtCore     import *
     from PySide2.QtGui      import *
     from PySide2.QtWidgets  import *
-    from PySide2.QtMultimedia import *
-    from PySide2.QtMultimediaWidgets import *
 except:
     from PySide.QtCore      import *
     from PySide.QtGui       import *
 
+import vlc
+
+
+from ...core.logger import get_logging
+logger = get_logging(__name__)
+
 class VideoButton(QWidget):
     """Video Button class.
+    (Initial work from: https://git.videolan.org/?p=vlc/bindings/python.git;a=blob;f=examples/pyqt5vlc.py;h=cb3d29488c9efe43a80ae8d17e083137b368487d;hb=HEAD )
 
     Args:
         name (str, optional): Text of the button. Defaults to "".
@@ -39,8 +45,48 @@ class VideoButton(QWidget):
         self._status = status
         self._functionToInvoke = functionToInvoke
 
+        # Create a basic vlc instance
+        self.instance = vlc.Instance()
+
+        self.mediaplayer = self.instance.media_player_new()
+
+        self.is_paused = False
+
         self.initUI()
-    
+
+        if(not os.path.isfile(self._iconPath)):
+            logger.warning("videoButton widget: file {} not found.".format(self._iconPath))
+        else:
+            # Add media to mediaplayer.
+            self.media = self.instance.media_new(self._iconPath)
+
+            # Put the media in the media player
+            self.mediaplayer.set_media(self.media)
+
+            # Parse media metadatas.
+            self.media.parse()
+
+            # The media player has to be 'connected' to the QFrame (otherwise the
+            # video would be displayed in it's own window). This is platform
+            # specific, so we must give the ID of the QFrame (or similar object) to
+            # vlc. Different platforms have different functions for this
+            if platform.system() == "Linux": # for Linux using the X Server
+                self.mediaplayer.set_xwindow(int(self.videoframe.winId()))
+            elif platform.system() == "Windows": # for Windows
+                self.mediaplayer.set_hwnd(int(self.videoframe.winId()))
+            elif platform.system() == "Darwin": # for MacOS
+                self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
+            
+            # Init display.
+            self.mediaplayer.set_position(0.0)
+
+            # Set video size.
+            self.videoframe.setFixedSize(self.mediaplayer.video_get_width()/4,
+                                        self.mediaplayer.video_get_height()/4)
+
+            # Ajust video scale to frame.
+            self.mediaplayer.video_set_scale(0)
+
     @property
     def iconScale(self):
         """Return icon scale in pixel.
@@ -62,19 +108,12 @@ class VideoButton(QWidget):
         if(event.type() == QEvent.MouseButtonPress):
             if(event.button() == Qt.LeftButton):
                 self._functionToInvoke()
-            if(event.button() == Qt.RightButton):
-                if(self.mediaPlayer.state() == QMediaPlayer.PlayingState):
-                    self.mediaPlayer.pause()
-                else:
-                    self.mediaPlayer.play()
 
     def enterEvent(self, event):
-        if(self.mediaPlayer.state() != QMediaPlayer.PlayingState):
-                self.mediaPlayer.play()
+        self.play()
     
     def leaveEvent(self, event):
-        self.mediaPlayer.pause()
-        self.mediaPlayer.setPosition(0)
+        self.stop()
 
     def initUI(self):
         """Main UI creation function.
@@ -86,25 +125,52 @@ class VideoButton(QWidget):
         self.mainLayout.setSpacing(0)
         self.mainLayout.setContentsMargins(0, 0, 0, 0)
 
-        # Create a video widget.
-        self.videoWidget = QVideoWidget()
-        # TODO: Mouve to dynamic.
-        self.videoWidget.setMinimumSize(100, 100)
+        self.videoframe = QFrame()
 
-
-        # Create a media player for the video.
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self._iconPath)))
-        self.mediaPlayer.setVideoOutput(self.videoWidget)
-        self.mediaPlayer.play()
-        self.mediaPlayer.pause()
-        self.mediaPlayer.setPosition(0)
-
-        #videoWidget.show()
-        self.mainLayout.addWidget(self.videoWidget)
+        self.palette = self.videoframe.palette()
+        self.palette.setColor(QPalette.Window, QColor(0, 0, 0))
+        self.videoframe.setPalette(self.palette)
+        self.videoframe.setAutoFillBackground(True)
+                                        
+        self.mainLayout.addWidget(self.videoframe)
 
         # Add the main layout to the window.
         self.setLayout(self.mainLayout)
+
+    def play_pause(self):
+        """Toggle play/pause status
+        """
+        if self.mediaplayer.is_playing():
+            self.mediaplayer.pause()
+            self.is_paused = True
+        else:
+            if self.mediaplayer.play() == -1:
+                return
+
+            self.mediaplayer.play()
+            self.is_paused = False
+    
+    def play(self):
+        """Toggle play status
+        """
+        if(not self.mediaplayer.is_playing()):
+            if self.mediaplayer.play() == -1:
+                return
+            
+            self.mediaplayer.play()
+            self.is_paused = False
+
+    def pause(self):
+        """Toggle pause status
+        """
+        if(self.mediaplayer.is_playing()):
+            self.mediaplayer.pause()
+            self.is_paused = True
+
+    def stop(self):
+        """Stop player
+        """
+        self.mediaplayer.stop()
     
     def changeButtonStatus(self, status):
         """Change button status.
